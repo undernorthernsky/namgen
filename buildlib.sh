40f1e77084f4018e74a03a17159cddec01ee95de
Tim Oliver Kaiser <under.northern.sky@gmail.com>
2010-11-24 12:31:36 +0100

    build libs w/o cmake; try for installed zziplib

--- buildlib.sh
+++ buildlib.sh
@@ -1,44 +1,67 @@
 #!/bin/bash
 
-mkdir include lib
+if [ ! -d include ]; then mkdir include ; fi
+if [ ! -d lib ]; then mkdir lib; fi
 T=$PWD
-cd ngtemplate/lib/libuseful/src
-cmake CMakeLists.txt
-make
-cp include/*.h $T/include
-cp libuseful.a $T/lib
-make clean
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
-cd $T
 
-cd ngtemplate/src
-cmake CMakeLists.txt
-make
-cp include/*.h $T/include
-cp libngtemplate.a $T/lib
-make clean
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
-cd $T
-
-if [ -e lib/libzzip.a ]; then exit 0; fi
-
-mkdir include/zzip
-
-if [ ! -e zziplib-0.13.58.tar.bz2 ]; then
-    wget http://sourceforge.net/projects/zziplib/files/zziplib13/0.13.58/zziplib-0.13.58.tar.bz2/download
+if [ ! -e lib/libuseful.a ]; then
+    echo "building: libuseful.a"
+    cd ngtemplate/lib/libuseful/src
+    for f in *.c; do
+        gcc -Os -Iinclude -c $f
+    done
+    ar cr libuseful.a *.o
+    cp include/*.h $T/include
+    mv libuseful.a $T/lib
+    rm *.o
+    cd $T
 fi
 
-if [ ! -d zziplib-0.13.58 ]; then
-    tar jxf zziplib-0.13.58.tar.bz2 
+if [ ! -e lib/libngtemplate.a ]; then
+    echo "building: libngtemplate.a"
+    cd ngtemplate/src
+    for f in *.c; do
+        gcc -Os -Iinclude -I../../include -c $f
+    done
+    ar cr libngtemplate.a *.o
+    rm *.o
+    cp include/*.h $T/include
+    mv libngtemplate.a $T/lib
+    cd $T
 fi
 
-D=zziplib-0.13.58 
+ld -lzzip1 2>/dev/null
+if [ $? -eq 1 ]; then
+    echo "building: libzzip"
+    #wget http://sourceforge.net/projects/zziplib/files/zziplib13/0.13.58/zziplib-0.13.58.tar.bz2/download
+    cp /home/tok/src/airepo/third_party/zziplib-0.13.58.tar.bz2 .
+    tar jxf zziplib-0.13.58.tar.bz2
+    D=zziplib-0.13.58
+    (cd $D && ./configure && make) >/dev/null 2>&1
+    mkdir include/zzip
+    cp $D/zzip/zzip.h include
+    cp $D/zzip/types.h $D/zzip/conf.h include/zzip
+    cp $D/Linux_*/zzip/_config.h include/zzip
+    cp $D/Linux_*/zzip/.libs/libzzip.a lib
 
-(cd $D && ./configure && make)
+    rm -rf $D zziplib-*.tar.bz2
+fi
+rm -f a.out
 
-cp $D/zzip/zzip.h include
-cp $D/zzip/types.h $D/zzip/conf.h include/zzip
-cp $D/Linux_*/zzip/_config.h include/zzip
-cp $D/Linux_*/zzip/.libs/libzzip.a lib
+#cd ngtemplate/lib/libuseful/src
+#cmake CMakeLists.txt
+#make
+#cp include/*.h $T/include
+#cp libuseful.a $T/lib
+#make clean
+#rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
+#cd $T
 
-rm -rf $D
+#cd ngtemplate/src
+#cmake CMakeLists.txt
+#make
+#cp include/*.h $T/include
+#cp libngtemplate.a $T/lib
+#make clean
+#rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
+#cd $T
